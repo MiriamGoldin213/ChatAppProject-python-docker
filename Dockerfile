2594646f710c74c5be0dda3671e72b28f3adc65a
Miriam Goldin <miriam.goldin@grunitech.com>
2023-09-26 17:31:14 +0300

    adding my project

---
+++ Dockerfile
@@ -0,0 +1,27 @@
+# set base image (host OS)
+FROM python:3.8-slim
+# --- NETFREE CERT INTSALL ---
+# ADD https://netfree.link/dl/unix-ca.sh /home/netfree-unix-ca.sh 
+# RUN cat  /home/netfree-unix-ca.sh | sh
+# ENV NODE_EXTRA_CA_CERTS=/etc/ca-bundle.crt
+# ENV REQUESTS_CA_BUNDLE=/etc/ca-bundle.crt
+# ENV SSL_CERT_FILE=/etc/ca-bundle.crt
+# --- END NETFREE CERT INTSALL ---
+# set the working directory in the container
+WORKDIR /chatApp
+# copy the dependencies file to the working directory
+COPY requirements.txt .
+# install dependencies
+RUN pip install -r requirements.txt
+# Configure environment
+ENV DATA_DIR='./data/'
+ENV ROOMS_DIR=${DATA_DIR}'rooms/'
+ENV TZ 'Israel'
+ENV FLASK_ENV development
+# copy the content of the local src directory to the working directory
+COPY . .
+# Helth check
+HEALTHCHECK CMD ["curl", "-f", "http://localhost:5000/health"] INTERVAL=10s TIMEOUT=3s
+# command to run on container start
+CMD [ "python", "./chatApp.py" ]
+
---
+++ scripts/deploy.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+# Check if the script was passed any arguments
+if [ $# -eq 0 ]; then
+    # No arguments were passed, so prompt the user for them
+    # Get the version and commit hash from the user
+    echo "Enter the version number: "
+    read version
+    echo "Enter the commit hash: "
+    read commit_hash
+else
+  # Arguments were passed, so use them
+  version=$1
+  commit_hash=$2
+fi
+
+#!/bin/bash
+
+version="v$version"
+dock_name='Dockerfile'
+
+if [ $# -eq 3 ]; then
+    dock_name=$3
+fi
+
+appname="chat-app"
+
+if docker image inspect "$appname:$version" &> /dev/null; then
+    read -p "Image $appname:$version already exists. Do you want to rebuild it? (y/n): " rebuild
+
+    if [[ "$rebuild" == "y" ]]; then
+        # Delete the existing image
+        docker rmi "$appname:$version"
+
+        # Build the Docker image
+        docker build -t "${appname}:${version}" . -f "${dock_name}"
+    else
+        echo "Using existing image $appname:$version."
+    fi
+else
+    docker build -t "${appname}:${version}" . -f "${dock_name}"
+fi
+
+read -p "Do you want to push the image to Artifact Registry? (y/n): " push_to_registry
+
+if [ "$push_to_registry" = "y" ]; then
+    # Set the impersonation service account
+    gcloud config set auth/impersonate_service_account artifact-admin-sa@grunitech-mid-project.iam.gserviceaccount.com
+
+    # Tag the Docker image for Artifact Registry
+    docker tag "${appname}:${version}" "me-west1-docker.pkg.dev/grunitech-mid-project/miriam-chat-app-images/chat-app:${version}"
+
+    # Push the Docker image to Artifact Registry
+    docker push "me-west1-docker.pkg.dev/grunitech-mid-project/miriam-chat-app-images/chat-app:${version}"
+
+    if [ $? -eq 0 ]; then
+        echo "Image successfully pushed to Artifact Registry."
+    else
+        echo "Error pushing image to Artifact Registry."
+        exit 1
+    fi
+else
+    echo "Image not pushed to Artifact Registry."
+fi
+
+
+
+read -p "Do you want to push the tag to GitHub? (y/n): " push_tag
+if [[ "$push_tag" == "y" ]]; then
+    # Tag the image with the commit hash
+    git tag ${version} ${commit_hash}
+    git push origin ${version}
+    # Check if the image was pushed successfully
+    if [ $? -ne 0 ]; then
+        echo "Error pushing image to GitHub"
+        exit 1
+    else
+        # Success!
+        echo "Image pushed to GitHub successfully"
+    fi
+else
+    echo "Tag not pushed to GitHub."
+fi
